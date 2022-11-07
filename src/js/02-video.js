8500c3f84d4ea049dcf7fb21b81aacdcd0695683
Antonina Buchak <antoninabucak@gmail.com>
2022-11-07 11:11:49 +0200

    add task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// player.on(
+//   'timeupdate',
+//   throttle(el => {
+//     localStorage.setItem('videoplayer-current-time', el.seconds);
+//   }, 1000)
+// );
+// player
+//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+//   .catch(function (error) {
+//     console.log(error);
+//   });
